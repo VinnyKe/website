991a730f031d7aff09eb8d6689a7c6c2beaa4180
VinnyKe <vnieutin.dev@gmail.com>
2021-11-27 17:16:23 +0100

    Initial commit

---
+++ website/resources/js/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container">
+        <h1>{{ welcomeMessage }}</h1>
+        <ExampleComponent></ExampleComponent>
+    </div>
+</template>
+
+<script>
+    import ExampleComponent from './components/ExampleComponent';
+
+    export default {
+        components: {
+            ExampleComponent
+        },
+        data(){
+            return {
+                welcomeMessage: 'Hello there!'
+            }
+        }
+    }
+
+</script>
